6b83ff581cff018e8819dee0d92a1d60a681b292
“claudio-code” <suporte2424@outlook.com>
2022-08-20 18:18:05 -0300

    added example to java

---
+++ java/src/main/java/org/example/binaryHeap/min/GFG.java
@@ -0,0 +1,104 @@
+package org.example.binaryHeap.min;
+
+import java.util.Arrays;
+
+public class GFG {
+    private int[] Heap;
+    private int size;
+    private int maxsize;
+    private static final int FRONT = 1;
+
+    public GFG(int maxsize)
+    {
+        this.maxsize = maxsize;
+        this.size = 0;
+        Heap = new int[this.maxsize + 1];
+        Heap[0] = Integer.MIN_VALUE;
+    }
+
+    private int parent(int pos) { return pos / 2; }
+
+    private int leftChild(int pos) { return (2 * pos); }
+
+    private int rightChild(int pos)
+    {
+        return (2 * pos) + 1;
+    }
+
+    private boolean isLeaf(int pos)
+    {
+
+        if (pos > (size / 2)) {
+            return true;
+        }
+
+        return false;
+    }
+
+    private void swap(int fpos, int spos)
+    {
+        int tmp;
+        tmp = Heap[fpos];
+        Heap[fpos] = Heap[spos];
+        Heap[spos] = tmp;
+    }
+
+    private void minHeapify(int pos)
+    {
+        if(!isLeaf(pos)){
+            int swapPos = Heap[leftChild(pos)]<Heap[rightChild(pos)]?leftChild(pos):rightChild(pos);
+            if(Heap[pos]>Heap[leftChild(pos)] || Heap[pos]> Heap[rightChild(pos)]){
+                swap(pos,swapPos);
+                minHeapify(swapPos);
+            }
+
+        }
+    }
+
+    public void insert(int element)
+    {
+        if (size >= maxsize) {
+            return;
+        }
+
+        Heap[++size] = element;
+        int current = size;
+        while (Heap[current] < Heap[parent(current)]) {
+            swap(current, parent(current));
+            current = parent(current);
+        }
+    }
+
+    public void print()
+    {
+        for (int i = 1; i <= size / 2; i++) {
+            System.out.print(
+                    " PARENT : " + Heap[i]
+                            + " LEFT CHILD : " + Heap[2 * i]
+                            + " RIGHT CHILD :" + Heap[2 * i + 1]);
+
+            System.out.println();
+        }
+    }
+
+    public int remove()
+    {
+        int popped = Heap[FRONT];
+        Heap[FRONT] = Heap[size--];
+        minHeapify(FRONT);
+        return popped;
+    }
+
+    public static void main(String[] arg)
+    {
+        System.out.println("The Min Heap is ");
+        GFG minHeap = new GFG(15);
+
+        minHeap.insert(20);
+        minHeap.insert(16);
+        minHeap.insert(25);
+        minHeap.insert(23);
+        minHeap.print();
+        System.out.println("The Min val is " + minHeap.remove());
+    }
+}
