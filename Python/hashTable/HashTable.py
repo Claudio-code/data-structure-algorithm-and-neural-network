96db7918689ea2ae41ecb9ea159d2431375e2efc
“claudio-code” <suporte2424@outlook.com>
2022-10-21 19:42:39 -0300

    added hash table example

---
+++ Python/hashTable/HashTable.py
@@ -0,0 +1,30 @@
+from msilib.schema import tables
+import re
+import sys
+
+class HashTable:
+    
+    def __init__(self, table_size):
+        if table_size < 1:
+            print('Error: table size must be positive number!')
+            sys.exit(1)
+        self.table_size = table_size
+        self.table = [[] for i in range(table_size)]
+    
+    def hash_func(self, key):
+        return key % self.table_size
+
+    def insert(self, key):
+        self.table[self.hash_func(key)].append(key)
+    
+    def show(self):
+        for linked_list in self.table:
+            if linked_list:
+                for key in linked_list:
+                    print('%d' % key, end='')
+                print('')
+
+    def search(self, key):
+        if key in self.table[self.hash_func(key)]:
+            return True
+        return False
---
+++ Python/hashTable/main.py
@@ -0,0 +1,9 @@
+from HashTable import HashTable
+
+hashTable = HashTable(9)
+
+hashTable.insert(2)
+hashTable.insert(34)
+hashTable.insert(12)
+hashTable.insert(10)
+hashTable.show()
